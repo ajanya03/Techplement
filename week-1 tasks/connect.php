7f6e7d95ecd9bdc72e35ff5956ff763a24e553f2
ajanya03 <130043690+ajanya03@users.noreply.github.com>
2024-09-03 12:57:04 +0530

    Update connect.php

---
+++ week-1 tasks/connect.php
@@ -0,0 +1,39 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "techplement";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname)
+if(!$conn){
+    die("Connection Failed : ".mysqli_connect_error());
+}
+else{
+    die("Connected successfully")
+}
+
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $pswd = $_POST['pswd'];
+    $retypepswd = $_POST['retypepswd'];
+    $mobile = $_POST['mobile'];
+    $gender = $_POST['gender'];
+
+
+$sql = "INSERT INTO WeekOneTask(firstName, lastName, email, pswd, retypepswd, mobile, gender)VALUES(?,?,?,?,?,?,?)"
+
+$stmt = mysqli_stmt_init($conn);
+
+if( ! mysqli_stmt_prepare($stmt,$sql) ){
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "ssdddis", $firstname, $lastname, $email, $pswd, $retypepswd, $mobile, $gender);
+
+mysqli_stmt_execute($stmt);
+
+echo "Data Saved Succesfully";
+
+?>
